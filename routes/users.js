fa0550f2f2487d20a602a2dbdb7557b2d83ccd9a
Sohel Rana <sohelcuetcse11@gmail.com>
2019-02-18 16:21:57 +0600

    restaurant api added

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,4 @@
 
-const _ = require("lodash");
 const express = require("express");
 const { Users } = require("../models/user");
 const { Restaurants } = require("../models/restaurant");
--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const users = require("../routes/users");
+const restaurants = require("../routes/restaurants");
 const error = require("../middleware/error");
 
 module.exports = function (app) {
   app.use(express.json());
   app.use("/api/users", users);
+  app.use("/api/restaurants", restaurants);
   app.use(error);
 };
